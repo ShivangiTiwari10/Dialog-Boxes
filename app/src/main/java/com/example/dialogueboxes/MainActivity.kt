87426b187aef5b89c1f65adacb0a05c78c96a956
Shivangi <shivangit1111@gmail.com>
2023-03-25 16:52:48 +0530

    feat: bind view

--- app/src/main/java/com/example/dialogueboxes/MainActivity.kt
+++ app/src/main/java/com/example/dialogueboxes/MainActivity.kt
@@ -2,10 +2,13 @@ package com.example.dialogueboxes
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.dialogueboxes.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
