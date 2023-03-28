fd79a2bb1489a7e440156a490f7f23c91656ef6b
Shivangi <shivangit1111@gmail.com>
2023-03-28 12:14:58 +0530

    feat: add alert dialog

--- app/src/main/java/com/example/dialogueboxes/Questions.kt
+++ app/src/main/java/com/example/dialogueboxes/Questions.kt
@@ -1,7 +1,10 @@
 package com.example.dialogueboxes
 
+import android.content.DialogInterface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import com.example.dialogueboxes.databinding.ActivityQuestionsBinding
 
 class Questions : AppCompatActivity() {
@@ -10,6 +13,28 @@ class Questions : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityQuestionsBinding.inflate(layoutInflater)
+
+
+        binding.question1.setOnClickListener {
+            val options = arrayOf("Python", "Java", "Kotlin","C++")
+            val builder = AlertDialog.Builder(this)
+            builder.setTitle("Which is your favourite Language?")
+            builder.setSingleChoiceItems(options,0,DialogInterface.OnClickListener { dialogInterface, i ->
+
+                Toast.makeText(this, "You clicked on ${options[i]}", Toast.LENGTH_SHORT).show()
+
+            })
+            builder.setPositiveButton(
+                "submit",
+                DialogInterface.OnClickListener { dialogInterface, i -> })
+
+            builder.setNegativeButton(
+                "Decline",
+                DialogInterface.OnClickListener { dialogInterface, i -> })
+
+
+            builder.show()
+        }
         setContentView(binding.root)
     }
 }
