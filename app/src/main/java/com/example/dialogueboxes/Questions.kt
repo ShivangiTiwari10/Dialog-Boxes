58b42ea42e7ab7079f79edd4dcf8f3104ca41838
Shivangi <shivangit1111@gmail.com>
2023-03-28 11:35:54 +0530

    feat: bind view

--- app/src/main/java/com/example/dialogueboxes/Questions.kt
+++ app/src/main/java/com/example/dialogueboxes/Questions.kt
@@ -2,10 +2,14 @@ package com.example.dialogueboxes
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.dialogueboxes.databinding.ActivityQuestionsBinding
 
 class Questions : AppCompatActivity() {
+
+    private lateinit var binding: ActivityQuestionsBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_questions)
+        binding = ActivityQuestionsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
